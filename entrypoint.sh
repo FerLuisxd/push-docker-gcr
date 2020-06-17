1c5ac01952c45f8374dd2e97acc609bfa5d9a6d5
Luis Vilca <ferluisxd@gmail.com>
2020-06-16 20:58:30 -0500

    feat(conditional)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,10 @@
 IMAGE_NAME="$INPUT_GCR_HOST/$GOOGLE_PROJECT_ID/$INPUT_IMAGE_NAME:$INPUT_IMAGE_TAG"
 
 echo $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
-docker build -t $IMAGE_NAME -t $INPUT_IMAGE_TAG2 .
+if [[ -z $INPUT_IMAGE_TAG2 ]]; then
+  docker build -t $IMAGE_NAME .
+else
+  docker build -t $IMAGE_NAME -t $INPUT_IMAGE_TAG2 .
+fi
+
 docker push $IMAGE_NAME
